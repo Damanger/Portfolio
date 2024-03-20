74851959bc589b77c457cee208cbcfb90752884a
Omar Rmz <omar.cruzr97@gmail.com>
2024-03-19 21:21:26 -0600

    Updating everything

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import React, { useState } from 'react';
 import Inicio from './componentes/inicio';
-import ARM from './componentes/ARM'
+import ARM from './componentes/ARM';
 import Assistant from './componentes/WebAssistant';
 import Error404 from './componentes/Error404';
-import Typing from './componentes/Typing'
+import Typing from './componentes/Typing';
 import Clock from './componentes/Clock';
 import Cargando from './componentes/Cargando';
 import Binary from './componentes/binary';
@@ -18,6 +17,7 @@ function App() {
   setTimeout(() => {
     setIsLoading(false);
   }, 1000);
+
   return (
     <Router>
       {isLoading ? (
@@ -36,20 +36,35 @@ function App() {
             <span></span>
             <span></span>
           </div>
-          <Clock/>
+          <Clock />
           <Routes>
-            <Route path='/' element={<Inicio/>} />
-            <Route path='/ARM' element={<ARM/>} />
-            <Route path='/Assistant' element={<Assistant/>} />
-            <Route path='/typing' element={<Typing/>} />
-            <Route path='/binary' element={<Binary/>} />
-            <Route path='/map' element={<Map/>} />
-            <Route path="*" element={<><Error404/><Navigate to="/not-found" replace /></>}/>
+            <Route path='/' element={<Inicio />} />
+            <Route path='/ARM' element={<ARM />} />
+            <Route path='/Assistant' element={<Assistant />} />
+            <Route path='/typing' element={<Typing />} />
+            <Route path='/binary' element={<Binary />} />
+            {/* En esta ruta, envuelve tu componente Map en un div que contenga los recursos */}
+            <Route path='/map' element={(
+              <>
+                <MapResources />
+                <Map />
+              </>
+            )} />
+            <Route path="*" element={<><Error404 /><Navigate to="/not-found" replace /></>} />
           </Routes>
         </div>
       )}
     </Router>
   );
 }
+
+// Componente que carga los recursos solo para la ruta /map
+const MapResources = () => (
+  <>
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossOrigin="" />
+    <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossOrigin=""></script>
+  </>
+);
 
 export default App;
