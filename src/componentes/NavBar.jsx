0a898c5b38b130687143dfb00a48188cb230351f
Omar Rmz <omar.cruzr97@gmail.com>
2024-03-19 22:41:50 -0600

    Updating everything

--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import '../assets/css/navbar.css';
 import { Link } from 'react-router-dom';
+import LazyLoad from 'react-lazy-load';
 
 const NavBar = () =>{
     return (
         <nav className="navbar2" style={{backgroundColor: "#3b82f6", display: "flex", justifyContent: "space-between", padding: "1rem ", height: "10%", alignItems: "center"}}>
             <Link to="/" className="logo" rel='noreferrer'>
-                <img className='logo-img' src="./omar.webp" alt="Omar Cruz" style={{ width: '15rem', height: 'auto' }}/>
+                <LazyLoad threshold={0.95}>
+                    <img className='logo-img' src="./omar.webp" alt="Omar Cruz" style={{ width: '15rem', height: 'auto' }}/>
+                </LazyLoad>
             </Link>
             <div className="cipher Project-Links" style={{fontSize: "1.75rem"}}>
                 <Link className="Link-Project" to="https://www.omar-cruz-rmz.com/map" target="_blank" rel='noreferrer'>
