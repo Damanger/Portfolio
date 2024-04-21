2771f3205d9c565eacfaa0c34ce6919a7d63b497
Damanger <omar.cruzr97@gmail.com>
2024-04-21 12:40:54 -0600

    updating cargando

--- src/componentes/Cargando.jsx
+++ src/componentes/Cargando.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../assets/css/cargando.css';
 
 const Cargando = ({ url }) => {
+    const [imageLoaded, setImageLoaded] = useState(false);
+
+    useEffect(() => {
+        const img = new Image();
+        img.onload = () => {
+            setImageLoaded(true);
+        };
+        img.src = url;
+    }, [url]);
+
     return (
-        <div className="cargando">
+        <div className={`cargando ${imageLoaded ? 'loaded' : ''}`}>
             <img src={url} alt="Cargando..." />
         </div>
     );
