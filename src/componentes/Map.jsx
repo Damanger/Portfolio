388d5f93f5d55f002e530bfebab5e100f75a3868
Damanger <omar.cruzr97@gmail.com>
2024-04-21 12:26:49 -0600

    updating map

--- src/componentes/Map.jsx
+++ src/componentes/Map.jsx
@@ -13,82 +13,89 @@ const Map = () => {
     const mapRef = useRef(null); // Se utiliza useRef para mantener una referencia al mapa
     const controlRef = useRef(null); // Se utiliza useRef para mantener una referencia al control de enrutamiento
     const [showModal, setShowModal] = useState(false);
+    const [showMap, setShowMap] = useState(false);
 
     useEffect(() => {
-        // Obtener la ubicación actual del usuario
-        navigator.geolocation.getCurrentPosition(position => {
-            const { latitude, longitude } = position.coords;
+        if (showMap) {
+            // Obtener la ubicación actual del usuario
+            navigator.geolocation.getCurrentPosition(position => {
+                const { latitude, longitude } = position.coords;
 
-            // Verificar si el mapa ya está inicializado
-            if (!mapRef.current) {
-                // Crear el mapa con la ubicación actual del usuario
-                const map = L.map('mi_mapa').setView([latitude, longitude], 16);
-                mapRef.current = map; // Asignar el mapa al ref
+                // Verificar si el mapa ya está inicializado
+                if (!mapRef.current) {
+                    // Crear el mapa con la ubicación actual del usuario
+                    const map = L.map('mi_mapa').setView([latitude, longitude], 16);
+                    mapRef.current = map; // Asignar el mapa al ref
 
-                // Mapa
-                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                }).addTo(map);
+                    // Mapa
+                    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    }).addTo(map);
 
-                // Ícono personalizado para marcadores
-                const customIcon = L.icon({
-                    iconUrl: markerIcon,
-                    iconSize: [38, 38],
-                    iconAnchor: [20, 40]
-                });
+                    // Ícono personalizado para marcadores
+                    const customIcon = L.icon({
+                        iconUrl: markerIcon,
+                        iconSize: [38, 38],
+                        iconAnchor: [20, 40]
+                    });
 
-                // Ícono personalizado para el primer marcador
-                const firstMarkerIcon = L.icon({
-                    iconUrl: yah,
-                    iconSize: [45, 45],
-                    iconAnchor: [20, 40]
-                });
+                    // Ícono personalizado para el primer marcador
+                    const firstMarkerIcon = L.icon({
+                        iconUrl: yah,
+                        iconSize: [45, 45],
+                        iconAnchor: [20, 40]
+                    });
 
-                // Crear control de enrutamiento
-                const control = L.Routing.control({
-                    waypoints: [
-                        L.latLng(latitude, longitude),
-                        L.latLng(latitude + 0.0005, longitude + 0.0005)
-                    ],
-                    routeWhileDragging: true,
-                    createMarker: function(waypointIndex, waypoint) {
-                        // Crea un marcador personalizado para cada waypoint
-                        let title;
-                        let markerIcon = customIcon;
-                        if (waypointIndex === 0) {
-                            title = "You are here";
-                            markerIcon = firstMarkerIcon; // Icono para el primer marcador
-                        } else if (controlRef.current && waypointIndex === controlRef.current.getWaypoints().length - 1) {
-                            title = "Destination";
-                        } else {
-                            title = `Stop #${waypointIndex}`;
+                    // Crear control de enrutamiento
+                    const control = L.Routing.control({
+                        waypoints: [
+                            L.latLng(latitude, longitude),
+                            L.latLng(latitude + 0.0005, longitude + 0.0005)
+                        ],
+                        routeWhileDragging: true,
+                        createMarker: function(waypointIndex, waypoint) {
+                            // Crea un marcador personalizado para cada waypoint
+                            let title;
+                            let markerIcon = customIcon;
+                            if (waypointIndex === 0) {
+                                title = "You are here";
+                                markerIcon = firstMarkerIcon; // Icono para el primer marcador
+                            } else if (controlRef.current && waypointIndex === controlRef.current.getWaypoints().length - 1) {
+                                title = "Destination";
+                            } else {
+                                title = `Stop #${waypointIndex}`;
+                            }
+                            const marker = L.marker(waypoint.latLng, {
+                                icon: markerIcon,
+                                draggable: true
+                            });
+                            marker.bindPopup(title);
+                            return marker;
                         }
-                        const marker = L.marker(waypoint.latLng, {
-                            icon: markerIcon,
-                            draggable: true
-                        });
-                        marker.bindPopup(title);
-                        return marker;
-                    }
-                }).addTo(map);
-                
-                controlRef.current = control; // Se asigna el control de enrutamiento al ref
+                    }).addTo(map);
+                    
+                    controlRef.current = control; // Se asigna el control de enrutamiento al ref
 
-                // Agregar control de geocodificación
-                const geocoder = L.Control.geocoder().addTo(map);
+                    // Agregar control de geocodificación
+                    const geocoder = L.Control.geocoder().addTo(map);
 
-                // Listener para el evento markgeocode
-                geocoder.on('markgeocode', function(e) {
-                    const latlng = e.geocode.center;
-                    // Actualizar el segundo waypoint
-                    control.setWaypoints([
-                        control.getWaypoints()[0].latLng,
-                        latlng
-                    ]);
-                });
-            }
-        });
-    }, []);
+                    // Listener para el evento markgeocode
+                    geocoder.on('markgeocode', function(e) {
+                        const latlng = e.geocode.center;
+                        // Actualizar el segundo waypoint
+                        control.setWaypoints([
+                            control.getWaypoints()[0].latLng,
+                            latlng
+                        ]);
+                    });
+                }
+            });
+        }
+    }, [showMap]);
+
+    const handleLocationButtonClick = () => {
+        setShowMap(true);
+    };
 
     const handleHelpButtonClick = () => {
         setShowModal(true);
@@ -102,10 +109,19 @@ const Map = () => {
         <>
             <NavBar/>
             <h1 style={{ display : 'flex', justifyContent : 'center', alignContent : 'center', color : 'aliceblue', marginTop : '1rem', marginBottom : '1rem', fontSize : '3rem'}}>Destination Discovery</h1>
-            <div id="mi_mapa" style={{ width: '100%', height: '500px', border : 'solid 2px #30ff00', color : 'black', zIndex : '90' }}></div>
-            <div className="btn2-box btns">
-                <button style={{position : 'absolute', top : '85vh', left : '50%', transform : 'translate(-50%, -50%)'}} className="help-button" onClick={handleHelpButtonClick}>Help</button>
-            </div>
+            {showMap && (
+                <>
+                    <div id="mi_mapa" style={{ width: '100%', height: '500px', border : 'solid 2px #30ff00', color : 'black', zIndex : '90' }}></div>
+                    <div className="btn2-box btns">
+                        <button style={{position : 'absolute', top : '85vh', left : '50%', transform : 'translate(-50%, -50%)'}} className="help-button" onClick={handleHelpButtonClick}>Help</button>
+                    </div>
+                </>
+            )}
+            {!showMap && (
+                <div className="location-button-container btn2-box btns" style={{ position : 'absolute', top : '50%', left : '50%', transform : 'translate(-50%, -50%)', display:'flex', justifyContent:'center', alignContent:'center', textAlign:'center' }}>
+                    <button className="location-button help-button" onClick={handleLocationButtonClick} >Allow Location Access</button>
+                </div>
+            )}
             {showModal && (
                 <div className="modal-background" onClick={handleCloseModal}>
                     <div className="modal-content" style={{background: 'aliceblue'}}>
